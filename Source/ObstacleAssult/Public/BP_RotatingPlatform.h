662a6d45e1d0f99bf61120c452ab05eb0a56d1d7
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-05-14 19:02:51 -0400

    Game
    Game

---
+++ Source/ObstacleAssult/Public/BP_RotatingPlatform.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MovingPlatform.h"
+#include "BP_RotatingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBSTACLEASSULT_API ABP_RotatingPlatform : public AMovingPlatform
+{
+	GENERATED_BODY()
+	
+};
